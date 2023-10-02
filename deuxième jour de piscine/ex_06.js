bbb00f6b1e4c0cfcbf33b7bba4cc6b5e4349b1f2
FRSseif <seif-allah.fares@epitech.digital>
2023-10-02 15:40:10 +0200

    Create ex_06.js

---
+++ deuxième jour de piscine/ex_06.js
@@ -0,0 +1,9 @@
+if  (bananasCount>0){
+    displayThisText ("Yummy!")
+}
+else if (bananasCount==0){
+    displayThisText ("Oh no there is no banana.")
+}
+else if (bananasCount<0){
+    displayThisText ("Do i owe you bananas")
+}
