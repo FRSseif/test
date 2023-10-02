d29aa52bec9ca5f10b1e0c32d649771b6cffc5c7
FRSseif <seif-allah.fares@epitech.digital>
2023-10-02 14:33:32 +0200

    Update ex_02.js

--- deuxième jour de piscine/ex_02.js
+++ deuxième jour de piscine/ex_02.js
@@ -2,4 +2,7 @@ let integer= 42;
 let string ="forty two";
 let bool = 42.42;
 let this_is_null = null;
-document.write(integer +"<br/>"+ string +"<br/>"+ bool +"<br/>"+ this_is_null);
+displayThisText(integer)
+displayThisText(string)
+displayThisText(bool)
+displayThisText(this_is_null)
