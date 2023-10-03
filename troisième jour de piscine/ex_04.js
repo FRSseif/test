3d8afa1e652a53b76e20e0a3f580854704558b76
FRSseif <seif-allah.fares@epitech.digital>
2023-10-03 11:40:07 +0200

    Create ex_04.js

---
+++ troisième jour de piscine/ex_04.js
@@ -0,0 +1,18 @@
+function calculator() {
+    if (action === "add") {
+        return addition();
+    } else {
+        return substraction();
+    }
+}
+
+function addition() {
+    return a + b;
+
+
+}
+
+function subtraction() {
+    return a - b;
+
+}
