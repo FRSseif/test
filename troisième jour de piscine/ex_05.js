a002c798ebed992d146eb009475e5e1fde4153b4
FRSseif <seif-allah.fares@epitech.digital>
2023-10-03 12:48:49 +0200

    Create ex_05.js

---
+++ troisième jour de piscine/ex_05.js
@@ -0,0 +1,5 @@
+function getBasketContent() {
+    let arrayfruits = ["strawberry","strawberry","strawberry","apple","apple","lime","lime","peach","pear", "pear"]
+    console.log(arrayfruits.length)
+    return arrayfruits
+}
