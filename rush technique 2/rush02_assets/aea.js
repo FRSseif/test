248ca2f30341ad274d485df1c8a484ac66f5c03f
FRSseif <seif-allah.fares@epitech.digital>
2023-11-14 15:02:40 +0100

    upd

--- rush technique 2/rush02_assets/aea.js
+++
@@ -1,7 +0,0 @@
-const priceRange = document.getElementById('priceRange');
-const priceValue = document.getElementById('priceValue');
-
-priceRange.addEventListener('input', (event) => {
-    const selectedValue = event.target.value;
-    priceValue.textContent = `$${selectedValue}`;
-});
